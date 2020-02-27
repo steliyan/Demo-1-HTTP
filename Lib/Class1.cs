6382916ebd9d2765f1dc37538bde6bb4437c86b7
Steliyan Stoyanov <steliyan@st6.io>
2020-02-27 22:44:47 +0200

    Add some work

--- Lib/Class1.cs
+++ Lib/Class1.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Elastic.Apm;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,12 @@ namespace Lib
 {
     public class Class1
     {
+        public void DoHttpWork()
+        {
+            AgentComponents ac;
+            Console.WriteLine("Begin work...");
+            var client = new HttpClient();
+            Console.WriteLine("Work done!");
+        }
     }
 }
--- Runner/Program.cs
+++ Runner/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Lib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,11 @@ namespace Runner
     {
         static void Main(string[] args)
         {
+            var c = new Class1();
+            c.DoHttpWork();
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
         }
     }
 }
